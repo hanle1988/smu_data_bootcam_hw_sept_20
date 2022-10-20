52c89506ebeb6d44f4476c9c7f2b274bf85f4b0a
hanle1988 <hle.smu@gmail.com>
2022-10-19 21:52:14 -0500

    python

--- 03-Python/Solution/PyBank/MainPyBank.py
+++ 03-Python/Solution/PyBank/MainPyBank.py
@@ -24,60 +24,60 @@ with open(csvpath, encoding='utf-8') as csvfile:
 
     for row in csvreader:
         print(row)
-        # rows += 1
-        # total +=int(row[1])
+        rows += 1
+        total +=int(row[1])
 
-        # #calculate the changes
-        # if rows !=1:
-        #     change = int(row[1]) - last_profit
-        #     tot_changes +=change
-        #     num_changes += 1
+        #calculate the changes
+        if rows !=1:
+            change = int(row[1]) - last_profit
+            tot_changes +=change
+            num_changes += 1
 
-        #     if (change > max_change):
-        #             max_change = change
-        #             max_month = row[0]
-        #     elif (change < min_change):
-        #             min_change = change
-        #             min_month = row[0]
-        #     else:
-        #         pass
+            if (change > max_change):
+                    max_change = change
+                    max_month = row[0]
+            elif (change < min_change):
+                    min_change = change
+                    min_month = row[0]
+            else:
+                pass
 
-        # #reset
-        # last_profit = int(row[1])
+        #reset
+        last_profit = int(row[1])
 
     
-# print("Financial Analysis")
-# print("--------------------------------------------------------------")
-# print(f"Total Month: {rows}")
-# print(f"Total: ${total}")
-# #print(num_changes)
-# print(f"Average Change: {tot_changes/ num_changes}")
-# print(f"Greatest Increase in Profits: {max_month} (${max_change})")
-# print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
+print("Financial Analysis")
+print("--------------------------------------------------------------")
+print(f"Total Month: {rows}")
+print(f"Total: ${total}")
+#print(num_changes)
+print(f"Average Change: {tot_changes/ num_changes}")
+print(f"Greatest Increase in Profits: {max_month} (${max_change})")
+print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
 
 
 
-# output = f"""Financial Analysis
-# ---------------------------------
-# Total Month: {rows}
-# Total: ${total}
-# Average Change: ${round(tot_changes/ num_changes,2)}
-# Greatest Increase in Profits: {max_month} (${max_change})
-# Greatest Decrease in Profits: {min_month} (${min_change})
+output = f"""Financial Analysis
+---------------------------------
+Total Month: {rows}
+Total: ${total}
+Average Change: ${round(tot_changes/ num_changes,2)}
+Greatest Increase in Profits: {max_month} (${max_change})
+Greatest Decrease in Profits: {min_month} (${min_change})
 
-# """
-# print(output)
-# with open('pybank_analysis.txt',"w") as f:
-#     f.write(output)
+"""
+print(output)
+with open('pybank_analysis.txt',"w") as f:
+    f.write(output)
 
 
 
-# # The total number of months included in the dataset
+# The total number of months included in the dataset
 
-# # The net total amount of "Profit/Losses" over the entire period
+# The net total amount of "Profit/Losses" over the entire period
 
-# # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
-# # The greatest increase in profits (date and amount) over the entire period
+# The greatest increase in profits (date and amount) over the entire period
 
-# # The greatest decrease in profits (date and amount) over the entire period
+# The greatest decrease in profits (date and amount) over the entire period
--- README.md
+++
@@ -1,2 +0,0 @@
-# smu_data_bootcam_hw_sept_20
-SMU Bootcamp HW
